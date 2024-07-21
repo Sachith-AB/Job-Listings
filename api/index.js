8701353d464cda40c0a9ecfc0f76bde97c57aa4b
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-07-21 10:01:51 +0530

    create sign in api route

--- api/index.js
+++ api/index.js
@@ -3,34 +3,33 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
-import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
 mongoose.connect("mongodb+srv://ilakshitha7921:ilakshitha7921@cluster0.gfhczos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => {
-    console.log('MongoDB is conected');
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+.then(() => {
+  console.log('MongoDB is conected');
+})
+.catch((err) =>
+{
+  console.log(err);
+});
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(4500, () => {
-  console.log('Server is running port 4500');
+app.listen(4500,() => {
+    console.log('Server is running port 4500');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -47,6 +46,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
-app.use('/api/post', postRoutes);
-
