cbafce7e24d3476986440cc812af6d8d9c20bb79
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-08-04 14:00:43 +0530

    commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Contact from './pages/Contact';
@@ -15,49 +14,67 @@ import PosterCompanyInfo from './components/Poster.CompanyInfo';
 import PosterPostjob from './components/Poster.Postjob';
 import PosterFoundInfo from './components/Poster.FoundInfo';
 import PosterSocialMediaInfo from './components/Poster.SocialMediaInfo';
-import PartTimeJobPost from './components/Post.PartTimeJobPost';
+
+import PartTimeJobPost from './components/Post.PartTimeJobPost'
 import FulltimeJobPost from './components/Post.FulltimeJobPost';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
 import UpdatePost from './pages/UpdatePost';
 
+import SeekerProfile from './components/Seeker.Profile';
+
+
+
+
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/create-post' element={<CreatePost/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/finish" element={<Finish />} />
-        <Route path="/poster-dashboard" element={<PosterDashboard />}>
-          <Route index element={<PosterDashOverview />} />
-          <Route path="employeeprofile" element={<PosterDashOverview />} />
-          <Route path="post-jobs" element={<PosterPostjob />}>
-            <Route index element={<PosterCompanyInfo />} />
-            <Route path="founding" element={<PosterFoundInfo />} />
-            <Route path="social-media" element={<PosterSocialMediaInfo />} />
-            <Route path="account-setting" element={<PosterCompanyInfo />} />
-          </Route>
-          <Route path="settings" element={<PosterSetting />}>
-            <Route index element={<PosterCompanyInfo />} />
-          </Route>
+  return <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/finish' element={<Finish />} />
+      <Route path='/poster-dashboard' element={<PosterDashboard />} >
+        <Route index element={<PosterDashOverview />} />
+        <Route path='employeeprofile' element={<PosterDashOverview />} />
+        <Route path='post-jobs' element={<PosterPostjob />} >
+          <Route index element={<PosterCompanyInfo />} />
+          <Route path='founding' element={<PosterFoundInfo />} />
+          <Route path='social-media' element={<PosterSocialMediaInfo />} />
+          <Route path='account-setting' element={<PosterCompanyInfo />} />
         </Route>
-        <Route path="/company" element={<PosterCompanyInfo />} />
-        <Route path="/seeker-dashboard" element={<SeekerDashboard />} />
-        <Route path="/parttime-job-post" element={<PartTimeJobPost />} />
-        <Route path="/fulltime-job-post" element={<FulltimeJobPost />} />
-        <Route path="/post-page/:postslug" element={<Post/>}/>
-        <Route path='/update-post/:postId' element={<UpdatePost/>} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+        <Route path='settings' element={<PosterSetting />}>
+          <Route index element={<PosterCompanyInfo />} />
+          <Route path='' element={<Home />} />
+          <Route path='about' element={<About />} />
+
+          <Route path='dashboard' element={<Dashboard />} />
+
+
+        </Route>
+      </Route>
+      <Route path='/company' element={<PosterCompanyInfo />} />
+
+      <Route path='/seeker-dashboard' element={<SeekerDashboard />} />
+      <Route path='/parttime-job-post' element={<PartTimeJobPost />} />
+      <Route path='/fulltime-job-post' element={<FulltimeJobPost />} />
+
+
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+
+
+
+
+   
+
+
+
 }
