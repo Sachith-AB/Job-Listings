7fbc8f23280ac9b2c5b4227030799bc821c3513e
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-08-05 14:22:06 +0530

    create home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,33 @@
-import { Button, TextInput } from 'flowbite-react';
-import React, { useEffect, useState } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { TbFilterBolt } from "react-icons/tb";
-import PostCards from '../components/PostCards';
+import React,{ useEffect, useState } from 'react'
+import {Link} from 'react-router-dom';
+import {Spinner} from 'flowbite-react';
 import SeekerCartPost from '../components/Seeker.cartPost';
+
 
 export default function Home() {
 
-  const [posts,setPosts] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const [showMore,setShowMore] = useState(false);
+    const [posts,setPosts] = useState([]);
+    useEffect(()=>{
+        const fetchPosts = async () => {
+            setLoading(true);
+            setShowMore(false);
+            const res = await fetch(`/api/post/get-posts`);
+            const data = await res.json();
+            if(data.length > 8){
+                setShowMore(true);
+            }
+            else{
+                setShowMore(false);
+            }
+                setPosts(data);
+                setLoading(false);
+        };
+        fetchPosts();
+    },[posts._id])
 
-  useEffect(() =>{
-    const fetchPosts = async () => {
-      const res = await fetch('/api/post/get-posts');
-      const data = await res.json();
-      setPosts(data.posts)
-      console.log(posts)
-    }
-    fetchPosts();
-  },[])
-
+  
   return (
     <div className="min-h-screen  ">
       <div className="flex justify-end p-4 ">
@@ -51,6 +60,32 @@ export default function Home() {
         )
       }
       
+    
+    <div>
+      <img src="https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/06/30125154/How-to-Get-a-Job-Where-You-Used-to-Work.jpg" alt="home page photo" 
+      className=''/>
     </div>
-  );
+
+    <div className=''>
+          <h1 className='text-3xl font-bold text-slate-700 p-4'>Listing result:</h1>
+          <div className='p-7 flex flex-wrap gap-4'>
+              {!loading && posts.length === 0 && (
+                      <p className='text-lg text-center text-slate-700'>No result found..!</p>
+                  )
+              }
+              {
+                  loading && ( 
+                     <Spinner size='xl'/>
+                  )
+              }
+
+            </div>
+          <div className='text-center mb-4'>
+          {showMore && (
+              <button onClick={onShowMoreClick} className='text-green-600 hover:underline'> Show More</button>
+          )}
+          </div>
+      </div>
+  </div>
+  )
 }
